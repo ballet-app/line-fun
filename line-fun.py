2c900709fb7d3451df22328d0da88412a00ff6e9
ballet-app <let.blanka@gmail.com>
2025-04-28 15:10:46 +0200

    Update line-fun.py

--- line-fun.py
+++ line-fun.py
@@ -1,33 +1,26 @@
+import streamlit as st
 import numpy as np
 import plotly.graph_objs as go
-import ipywidgets as widgets
-from IPython.display import display
 
-# Przygotuj dane początkowe
+# Tytuł aplikacji
+st.title("Wykres funkcji liniowej: f(x) = ax + b")
+
+# Suwaki dla parametrów a i b
+a = st.slider('Wybierz współczynnik a:', -10.0, 10.0, 1.0, step=0.1)
+b = st.slider('Wybierz współczynnik b:', -20.0, 20.0, 0.0, step=0.1)
+
+# Dane wykresu
 x = np.linspace(-10, 10, 400)
-y = 1 * x + 0
+y = a * x + b
 
-# Utwórz początkowy wykres
-fig = go.FigureWidget()
-fig.add_scatter(x=x, y=y, mode='lines')
-fig.update_layout(title='Wykres funkcji liniowej: f(x) = ax + b',
+# Tworzenie wykresu
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name=f'f(x) = {a}x + {b}'))
+fig.update_layout(title=f'Wykres funkcji: f(x) = {a}x + {b}',
                   xaxis_title='x',
                   yaxis_title='f(x)',
-                  template='plotly_white')
+                  template='plotly_white',
+                  showlegend=False)
 
-# Funkcja aktualizująca dane wykresu
-def update_plot(a, b):
-    with fig.batch_update():
-        fig.data[0].y = a * x + b
-        fig.layout.title = f'Wykres funkcji liniowej: f(x) = {a}x + {b}'
-
-# Suwaki
-a_slider = widgets.FloatSlider(value=1.0, min=-10.0, max=10.0, step=0.1, description='a:')
-b_slider = widgets.FloatSlider(value=0.0, min=-20.0, max=20.0, step=0.1, description='b:')
-
-# Połącz suwaki z funkcją aktualizującą
-ui = widgets.VBox([a_slider, b_slider])
-out = widgets.interactive_output(update_plot, {'a': a_slider, 'b': b_slider})
-
-# Wyświetl
-display(ui, fig, out)
+# Wyświetlenie wykresu
+st.plotly_chart(fig)
